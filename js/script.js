9c5bb4db614464c8d733ec46f5e9808c73f3ea8f
mac-saitou <ssssttt0925ts@gmail.com>
2023-08-27 23:03:32 +0900

    disabled完了

--- js/script.js
+++ js/script.js
@@ -47,3 +47,40 @@ $('.faqs-box').on('click', function(){
   $(this).children('.faqs-answer').slideToggle();
   $(this).children('.faqs-question').toggleClass('is-open');
 });
+
+
+
+
+$(document).ready(function() {
+  let $check = $('#js-checkbox');
+  let $submit = $('#js-submit-button');
+  
+  // 初期状態ではチェックボックスを無効化
+  $check.prop('disabled', true);
+  
+  // 入力フィールドの変更を監視
+  $('#js-name, #js-ruby').on('input', function() {
+    // 入力フィールドが空でない場合
+    if ($('#js-name').val() !== '' && $('#js-ruby').val() !== '') {
+      // チェックボックスを操作可能にする
+      $check.prop('disabled', false);
+    } else {
+      // チェックボックスを操作不可にし、チェックを外す
+      $check.prop('disabled', true);
+      $check.prop('checked', false);
+      // 送信ボタンを無効化
+      $submit.prop('disabled', true);
+    }
+  });
+  
+  // チェックボックスのクリックを監視
+  $check.on('click', function() {
+    if ($(this).prop('checked')) {
+      // 送信ボタンを有効化
+      $submit.prop('disabled', false);
+    } else {
+      // 送信ボタンを無効化
+      $submit.prop('disabled', true);
+    }
+  });
+});
